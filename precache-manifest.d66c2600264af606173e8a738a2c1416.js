362bc1341f9c7a6c9bf77e553d63c20d09fb8388
Aleksandr Shatokhin <aleks_shatoh@mail.ru>
2020-09-21 17:49:03 +0300

    Updates

---
+++ precache-manifest.d66c2600264af606173e8a738a2c1416.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb37e3b32405c6d2333fe01033833505",
+    "url": "/country-app/index.html"
+  },
+  {
+    "revision": "779f8cbafd1c1e8ad169",
+    "url": "/country-app/static/css/main.42935188.chunk.css"
+  },
+  {
+    "revision": "3361482104a75a293ec7",
+    "url": "/country-app/static/js/2.9655a504.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/country-app/static/js/2.9655a504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779f8cbafd1c1e8ad169",
+    "url": "/country-app/static/js/main.70fe100c.chunk.js"
+  },
+  {
+    "revision": "c49713e79af022656283",
+    "url": "/country-app/static/js/runtime-main.85689c85.js"
+  },
+  {
+    "revision": "08d4f989b14029dc7df9e6235e30243f",
+    "url": "/country-app/static/media/indian4.08d4f989.png"
+  },
+  {
+    "revision": "1a0574158921318262be982e4e179992",
+    "url": "/country-app/static/media/left.1a057415.svg"
+  }
+]);
